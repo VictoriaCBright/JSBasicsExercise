ad15131fbeb0831f6a3a61bf06fc4991c84ed2f5
Victoria Bright <victoriacbright92@gmail.com>
2022-09-27 09:35:22 -0500

    corrected

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 console.log(3 + 4)
 
-// 1. Numbers 2. Null 3. Booleans 4. String 5. Undefined
+// 1. Numbers 
+// 2. Null 
+//3. Booleans 
+//4. String 
+//5. Undefined
 
-// (), **, *, /, +, -
+// Exponentiation ** 
+//  Modulus is %
+// Multiplication is  *
+// Division is /
+// Addition is+
+// Subtraction is-
+// EXTRAS
+// increment ++
+// decrement --
 
-console.log(1%4)
-console.log(3%2)
+console.log(5 % 2) //1
+console.log(5 % 4) //1
 
 console.log('I\'M MARY POPPINS, Y\'ALL!')
 
 console.log("Hawk" + "eye")
 
-console.log("Victoria Bright".length)
+console.log("Victoria".length + "Bright".length)
 
 console.log("Avengers: Endgame" [16])
 
 // Bonus 1 come back to this (unsure)
-console.log("/")
+console.log("\/")
 
 //bonus 2
 console.log(5 ** 4 - 9)
 
 //bonus 3
-console.log((1 % 2) - 4 + 3)
-//SEE ABOUT THIS console.log((1-2) +4 -3)
+console.log((1-2) + 4 -3 )
+// console.log("I".length - "AM".length + "IRON".length - "MAN".length)
+
+// console.log("I".length - "AM".length + "IRON".length - "MAN".length % 10)
+// 1 + 2 + 4 + 3 = 10
+//10 % 10 = 0
 
 //bonus 4
 console.log("CAP"[1])
@@ -37,7 +53,7 @@ console.log("STAR-LORD"[0])
 
 //bonus 5 come back to this
 //console.log(m=25+c=4)
-var x = 25;
+var m = 25;
 var c = 4;
-var u = x + c;
-console.log(c + x ,  u)
+var u = m + c;
+console.log(u)
