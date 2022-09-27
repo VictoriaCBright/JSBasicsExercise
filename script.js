a5174e47811c6f6bcdd1e2d728fb1e4ae0d3e976
Victoria Bright <victoriacbright92@gmail.com>
2022-09-26 22:39:02 -0500

    completed but revisit

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log(3 + 4)
+
+// 1. Numbers 2. Null 3. Booleans 4. String 5. Undefined
+
+// (), **, *, /, +, -
+
+console.log(1%4)
+console.log(3%2)
+
+console.log('I\'M MARY POPPINS, Y\'ALL!')
+
+console.log("Hawk" + "eye")
+
+console.log("Victoria Bright".length)
+
+console.log("Avengers: Endgame" [16])
+
+// Bonus 1 come back to this (unsure)
+console.log("/")
+
+//bonus 2
+console.log(5 ** 4 - 9)
+
+//bonus 3
+console.log((1 % 2) - 4 + 3)
+//SEE ABOUT THIS console.log((1-2) +4 -3)
+
+//bonus 4
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("OKOYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[0])
+console.log("DOCTOR STRANGE"[13])
+console.log("THOR"[3])
+console.log("STAR-LORD"[0])
+
+//bonus 5 come back to this
+//console.log(m=25+c=4)
+var x = 25;
+var c = 4;
+var u = x + c;
+console.log(c + x ,  u)
